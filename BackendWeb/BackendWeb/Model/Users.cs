3da71de0b996f121f9b053cd8937c3eb90d72c59
serg1231-git <markusanon5999@gmail.com>
2021-10-24 22:26:42 +0300

    Model Users

---
+++ BackendWeb/BackendWeb/Model/Users.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BackendWeb.Model
+{
+    public class Users
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Column(TypeName ="varchar(25)")]
+        public string Login { get; set; }
+        
+        [Column(TypeName = "varchar(30)")]
+        public string Password { get; set; }
+
+        [Column(TypeName = "varchar(25)")]
+        public string Email { get; set; }
+        
+        [Column(TypeName ="bit")]
+        public bool rememberMe { get; set; }
+
+        [Column(TypeName = "varchar(20)")]
+        public string accessLevel { get; set; }
+    }
+}
