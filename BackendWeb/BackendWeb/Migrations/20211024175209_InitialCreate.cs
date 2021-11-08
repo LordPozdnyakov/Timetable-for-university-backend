bf78cb9d36402c21375d65881974ad5987a72d41
serg1231-git <markusanon5999@gmail.com>
2021-11-08 17:05:22 +0200

    Migrations datatase

---
+++ BackendWeb/BackendWeb/Migrations/20211024175209_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackendWeb.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Login = table.Column<string>(type: "varchar(25)", nullable: true),
+                    Password = table.Column<string>(type: "varchar(30)", nullable: true),
+                    Email = table.Column<string>(type: "varchar(25)", nullable: true),
+                    rememberMe = table.Column<bool>(type: "bit", nullable: false),
+                    accessLevel = table.Column<string>(type: "varchar(20)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
---
+++ BackendWeb/BackendWeb/Migrations/20211105121732_Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackendWeb.Migrations
+{
+    public partial class Student : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    Id_student = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Surname = table.Column<string>(type: "varchar(50)", nullable: true),
+                    Name = table.Column<string>(type: "varchar(30)", nullable: true),
+                    Patronymic = table.Column<string>(type: "varchar(25)", nullable: true),
+                    Date_birth = table.Column<DateTime>(type: "date", nullable: false),
+                    Place_residence = table.Column<string>(type: "varchar(50)", nullable: true),
+                    Telephone = table.Column<string>(type: "varchar(25)", nullable: true),
+                    Email = table.Column<string>(type: "varchar(20)", nullable: true),
+                    FullName_father = table.Column<string>(type: "varchar(60)", nullable: true),
+                    Father_telephone = table.Column<string>(type: "varchar(25)", nullable: true),
+                    FullName_mother = table.Column<string>(type: "varchar(60)", nullable: true),
+                    Mother_telephone = table.Column<string>(type: "varchar(25)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.Id_student);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
