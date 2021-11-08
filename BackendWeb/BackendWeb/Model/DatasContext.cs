c68db0df4b40b3473e918180146f18cd17a236fc
serg1231-git <markusanon5999@gmail.com>
2021-11-08 17:06:49 +0200

    Models for project

---
+++ BackendWeb/BackendWeb/Model/DatasContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendWeb.Model
+{
+    public class DatasContext:DbContext
+    {
+        public DatasContext(DbContextOptions<DatasContext> options):base (options)
+        {
+
+        }
+        public DbSet<Users> User { get; set; }
+        public DbSet<Student> Student { get; set; }
+    }
+}
---
+++ BackendWeb/BackendWeb/Model/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendWeb.Model
+{
+    public class Student
+    {
+        [Key]
+        public int Id_student { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string Surname { get; set; }
+
+        [Column(TypeName = "varchar(30)")]
+        public string Name { get; set; }
+
+        [Column(TypeName = "varchar(25)")]
+        public string Patronymic { get; set; }
+        
+        [Column(TypeName ="date")]
+        public DateTime Date_birth { get; set; }
+        
+        [Column(TypeName ="varchar(50)")]
+        public string Place_residence { get; set; }
+        
+        [Column(TypeName ="varchar(25)")]
+        public string Telephone { get; set;}
+
+        [Column(TypeName ="varchar(20)")]
+        public string Email { get; set; }
+        
+        [Column(TypeName ="varchar(60)")]
+        public string FullName_father { get; set; }
+        
+        [Column(TypeName ="varchar(25)")]
+        public string Father_telephone { get; set; }
+
+        [Column(TypeName = "varchar(60)")]
+        public string FullName_mother { get; set; }
+
+        [Column(TypeName = "varchar(25)")]
+        public string Mother_telephone { get; set; }
+    }
+}
